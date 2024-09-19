97818494623c8cc4243657ea5818c0fa0706c473
Kimble Culley <kculley@ut.utm.edu>
2024-09-19 01:34:58 -0500

    Changed sprintf to snprintf to mitiage buffer overflow

--- demo.c
+++ demo.c
@@ -130,9 +130,9 @@ static void DoBlocks(Display *display, Window window)
 
     /* Construct the demo level filename */
     if ((str = getenv("XBOING_LEVELS_DIR")) != NULL)
-        sprintf(levelPath, "%s/demo.data", str);
+      snprintf(levelPath, sizeof(levelPath), "%s/demo.data", str); //Changed sprintf to snprintf to prevent buffer overflow
     else
-        sprintf(levelPath, "%s/demo.data", LEVEL_INSTALL_DIR);
+      snprintf(levelPath, sizeof(levelPath), "%s/demo.data", LEVEL_INSTALL_DIR);
 
 	/* Read in a demo level */
     if (ReadNextLevel(display, window, levelPath, True) == False)
