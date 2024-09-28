29dc58d5146b9bfb5ac6f1c9bcb0127fc52543d8
josiahtripp <131731962+josiahtripp@users.noreply.github.com>
2024-09-28 14:42:44 -0500

    Doxygenated ulaw.h

--- include/ulaw.h
+++ include/ulaw.h
@@ -20,6 +20,7 @@
 
 #define UlToLin(_x) (ulawToLinearTable[(_x)])
 
+// [DOXYGEN] Mu-law algorithm, table used for converting from Mu-law to 16-bit PCM
 static short ulawToLinearTable[] =
 {
     0x8284, 0x8684, 0x8a84, 0x8e84, 0x9284, 0x9684, 0x9a84, 0x9e84,
@@ -56,7 +57,7 @@ static short ulawToLinearTable[] =
     0x0038, 0x0030, 0x0028, 0x0020, 0x0018, 0x0010, 0x0008, 0x0000,
 };
 
-
+// [DOXYGEN] Low-level sound properties used for audio playback
 typedef struct 
 {
     unsigned int  magic;
