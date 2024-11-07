d2c8e0b096160e70017137518773fc74b5654708
Jason Moss <jmoss35@ut.utm.edu>
2024-11-07 08:36:21 -0600

    added code for KeySym and XEvent into file

---
+++ include/faketypes.h
@@ -0,0 +1,11 @@
+// TODO: typedefs we have to make to get past compiler errors, these all
+//       indicate things we need to change
+#ifndef FAKETYPES_H
+#define FAKETYPES_H
+typedef int Display;
+typedef int Window;
+typedef int Colormap;
+typedef unsigned long int u_long;
+typedef int KeySym;
+typedef int XEvent;
+#endif
