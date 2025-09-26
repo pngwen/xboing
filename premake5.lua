e33cf4719736b128bc4757772fe28288819f38f6
zahcwa <zwallick@ut.utm.edu>
2025-09-25 22:45:36 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,31 @@
+function useraylib()
+    includedirs { os.findheader("raylib") }
+    libdirs { os.findlib("raylib") }
+    links { "raylib" }
+
+    filter "system:macosx" -- Configured for use with homebrew.
+        links {}
+        buildoptions { "`pkg-config raylib --cflags`" }
+        linkoptions { "`pkg-config raylib --libs`" }
+    filter {}
+end
+
 workspace "xboing"
     configurations { "Debug", "Release" }
     includedirs { "include" }
 
     filter "configurations:Debug"
         symbols "On"
-    
     filter "configurations:Release"
         optimize "On"
-    
-    filter "system:macosx"
-        buildoptions { "`pkg-config raylib --cflags`" }
-        linkoptions { "`pkg-config raylib --libs`" }
-    
     filter {}
-    
+
     project "Rayboing"
         kind "WindowedApp"
         language "C"
         targetdir "bin/%{cfg.buildcfg}"
-        
-        files { "src/rayboing/*.c" }
-        files { "src/paddle.c" }
-        removefiles { "src/rayboing/blockloader.c", "src/rayboing/paddle_demo.c" }
-        
-        includedirs { os.findheader("raylib") }
-        libdirs { os.findlib("raylib") }
-        links { "raylib" }
+
+        files { "src/rayboing/*.c", "src/paddle.c", "include/**.h" }
+        removefiles { "src/rayboing/blockloader.c", "src/rayboing/paddle_demo.c", "src/main.c" }
+
+        useraylib()
