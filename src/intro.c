f6258d8e24461ffe0b96f52b11d0170c7a059588
Dylan Pruitt <dpruitt6@ut.utm.edu>
2025-11-27 18:33:39 -0600

    Hopefully finished, Unanimated intro

--- src/intro.c
+++ src/intro.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include "intro.h"
 #define INTRO_TEXTURES "resource/textures/presents/"
-//TODO: Add star animation frame
+//TODO: Add star animation frames
 #define STAR_TEXTURES "resource/textures/stars/"
 //TODO: Add space background
 #define BACKGROUNDS "resource/textures/bgrnds/"
@@ -12,7 +12,7 @@ Texture2D introPlanet,
           introFlag,
           introJustin,
           introKibell,
-          introPresents,
+          //TODO: animate after "Justin Kibell" introPresents,
           intro_X,
           intro_B,
           intro_O,
@@ -33,8 +33,11 @@ bool loadIntroTextures(void) {
     if (introJustin.id == 0) return false;
     introKibell = LoadTexture(INTRO_TEXTURES "kibell.png");
     if (introKibell.id == 0) return false;
+    /*
+    //TODO: Animate after Justin Kibell
     introPresents = LoadTexture(INTRO_TEXTURES "presents.png");
     if (introPresents.id == 0) return false;
+    */
     intro_X = LoadTexture(INTRO_TEXTURES "titleX.png");
     if (intro_X.id == 0) return false;
     intro_B = LoadTexture(INTRO_TEXTURES "titleB.png");
@@ -71,22 +74,37 @@ void ShowIntroScreen(void) {
             Color introTint = WHITE;
             
             DrawTexture(introPlanet, 100, 50, introTint);
-            DrawTexture(introFlag,   100, 50, introTint);
-            DrawTexture(introJustin, 100, 50, introTint);
-            DrawTexture(introKibell, 100, 50, introTint);
-            DrawTexture(introPresents, 100, 50, introTint);
-            DrawTexture(intro_X, 100, 50, introTint);
-            DrawTexture(intro_B, 100, 50, introTint);
-            DrawTexture(intro_O, 100, 50, introTint);
-            DrawTexture(intro_I, 100, 50, introTint);
-            DrawTexture(intro_N, 100, 50, introTint);
-            DrawTexture(intro_G, 100, 50, introTint);   
+            DrawTexture(introFlag, (SCREEN_WIDTH - introFlag.width)/2, 20, WHITE);
+            DrawTexture(introJustin, (SCREEN_WIDTH - introJustin.width)/2,
+            SCREEN_HEIGHT - introKibell.height - introJustin.height - 30, WHITE);
+            DrawTexture(introKibell, (SCREEN_WIDTH - introKibell.width)/2, SCREEN_HEIGHT - introKibell.height - 20, WHITE);
+            //DrawTexture(introPresents, 100, 50, introTint); //TODO: Animate after Justin Kibell
+            //TODO: Animate letters coming in one_by_one
 
-            //const char *prompt = "Press ENTER or SPACE to Start";
-            //int promptSize = 20;
-            //int promptWidth = MeasureText(prompt, promptSize);
-            //DrawText(prompt, (SCREEN_WIDTH - promptWidth)/2, SCREEN_HEIGHT/3 + 80, promptSize, LIGHTGRAY);
+            int xboingY = (SCREEN_HEIGHT / 2) - 80; //Verticle line where "XBOING" sits
+            int spacing = 10; //Spacing in between "XBOING" letters
+            int totalWidth =
+                intro_X.width + intro_B.width + intro_O.width +
+                intro_I.width + intro_N.width + intro_G.width +
+                spacing * 5;
+
+            int startX = (SCREEN_WIDTH - totalWidth) / 2;
+            int x = startX;
+
+            DrawTexture(intro_X, x, xboingY, WHITE); x += intro_X.width + spacing;
+            DrawTexture(intro_B, x, xboingY, WHITE); x += intro_B.width + spacing;
+            DrawTexture(intro_O, x, xboingY, WHITE); x += intro_O.width + spacing;
+            DrawTexture(intro_I, x, xboingY, WHITE); x += intro_I.width + spacing;
+            DrawTexture(intro_N, x, xboingY, WHITE); x += intro_N.width + spacing;
+            DrawTexture(intro_G, x, xboingY, WHITE);
+
+            const char *prompt = "Press ENTER or SPACE to Start";
+            int promptSize = 20;
+            int promptWidth = MeasureText(prompt, promptSize);
+            DrawText(prompt, (SCREEN_WIDTH - promptWidth)/2, SCREEN_HEIGHT/3 + 80, promptSize, LIGHTGRAY);
         EndDrawing();
+
+        
 
         if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_KP_ENTER) || IsKeyPressed(KEY_SPACE)) {
             // Start requested
